d5a9786876d237a9bf9c18714e6b720c1939358e
Lucasza7 <133224905+Lucasza7@users.noreply.github.com>
2024-06-20 01:39:13 +0200

    klaar

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            description: 'Product Image',
-            url: 'https://example.com',
-            isActive: 'active',
-            product: 'Product Name'
+            cart:[],
+            premium: true
+        }
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
         }
     }
+    
 })
